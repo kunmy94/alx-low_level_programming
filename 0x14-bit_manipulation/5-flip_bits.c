9e69e1f1d96c9efbd029c15b934df44d821cbb36
Adelakun <kunmyboi94@gmail.com>
2023-10-27 04:58:39 -0700

    flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - count bit number
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int b, countbit = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (b = 63; b >= 0; b--)
+	{
+		current = exclusive >> b;
+		if (current & 1)
+			countbit++;
+	}
+
+	return (countbit);
+}
+
